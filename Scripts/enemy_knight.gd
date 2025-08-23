8bb8e3e665fbc94ba9ac133fe053ebe03aac9c68
Stary404 <mironicavasile@gmail.com>
2025-08-23 22:51:51 +0300

    Sprites

--- Scripts/enemy_knight.gd
+++ Scripts/enemy_knight.gd
@@ -4,8 +4,6 @@ const STARTING_HEALTH := 100.0
 const SPEED_X := 30.0
 const MAXIMUM_MOVEMENT_AREA := 50.0
 
-@onready var animated_sprite := $AnimatedSprite2D
-
 @onready var raycast_right = $RayCast2D_Right
 @onready var raycast_left = $RayCast2D_Left
 
@@ -26,18 +24,18 @@ func _process(delta: float) -> void:
 		queue_free()
 		return
 
-	var delta_x := delta * SPEED_X * direction
+	var delta_x := delta * SPEED_X
 
-	position.x += delta_x
-	movement_area += abs(delta_x)
+	position.x += delta_x * direction
+	movement_area += delta_x
 
 	var do_flip := false
 
 	if direction == 1:
-		if raycast_right.is_colliding() or !raycast_right_down.is_colliding():
+		if raycast_right.is_colliding() or not raycast_right_down.is_colliding():
 			do_flip = true
 	else:
-		if raycast_left.is_colliding() or !raycast_left_down.is_colliding():
+		if raycast_left.is_colliding() or not raycast_left_down.is_colliding():
 			do_flip = true
 
 	if movement_area >= MAXIMUM_MOVEMENT_AREA:
@@ -49,13 +47,17 @@ func _process(delta: float) -> void:
 func take_damage(_other: Node2D, hit_points: int) -> void:
 	health_points -= hit_points
 
-	animated_sprite.play("damaged")
+	for sprite in $Sprites.get_children():
+		sprite.play("damaged")
 
 func flip() -> void:
 	direction *= -1
-	animated_sprite.flip_h = !animated_sprite.flip_h
 	movement_area = 0.0
+
+	for sprite in $Sprites.get_children():
+		sprite.flip_h = not sprite.flip_h
 
 
 func _on_animated_sprite_2d_animation_finished() -> void:
-	animated_sprite.play("idle")
+	for sprite in $Sprites.get_children():
+		sprite.play("idle")
