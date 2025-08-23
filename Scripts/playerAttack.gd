49ba7e08a2c54ed41817e592672ce091a46262ae
Balea Andrei - Petru <balea_andrei@yahoo.com>
2025-08-23 23:02:34 +0300

    Player animation, attack and covidhealth pickable

--- Scripts/playerAttack.gd
+++ Scripts/playerAttack.gd
@@ -12,10 +12,8 @@ func _input(event: InputEvent) -> void:
 	if (event.is_action("attack")):
 		for enemy in targetedEnemies:
 			# damage the enemy
-			print("I'm trying to attack ", enemy.name)
 			if enemy.has_method("take_damage"):
 				enemy.take_damage(self, lightDamage)
-				print("I'm attacking " + enemy.name)
 
 
 func _on_body_entered(body: Node2D) -> void:
